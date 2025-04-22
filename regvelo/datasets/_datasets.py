6a64a40c8c660d6fd57ec006023fce0d676b59e1
WWXKenmo <ken71198@hotmail.com>
2025-04-22 22:35:49 +0200

    Add new dataset into regvelo

--- regvelo/datasets/_datasets.py
+++ regvelo/datasets/_datasets.py
@@ -10,6 +10,7 @@ from scvelo.read_load import load
 
 url_adata = "https://drive.google.com/uc?id=1Nzq1F6dGw-nR9lhRLfZdHOG7dcYq7P0i&export=download"
 url_grn = "https://drive.google.com/uc?id=1ci_gCwdgGlZ0xSn6gSa_-LlIl9-aDa1c&export=download/"
+url_adata_murine = "https://drive.google.com/uc?id=19bNQfW3jMKEEjpjNdUkVd7KDTjJfqxa5&export=download"
 
 def zebrafish_nc(file_path: Union[str, Path] = "data/zebrafish_nc/adata_zebrafish_preprocessed.h5ad"):
     """Zebrafish neural crest cells.
@@ -53,3 +54,22 @@ def zebrafish_grn(file_path: Union[str, Path] = "data/zebrafish_nc/prior_GRN.csv
     grn = pd.read_csv(url_grn, index_col = 0)
     grn.to_csv(file_path)
     return grn
+
+def murine_nc(file_path: Union[str, Path] = "data/murine_nc/adata_zebrafish_preprocessed.h5ad"):
+    """Mouse neural crest cells.
+
+    Single cell RNA-seq datasets of mouse neural crest cell development subset from Qiu, Chengxiang, et al. datasets.
+
+    The GRN is saved in `adata.uns["skeleton"]`, which learned via pySCENIC
+
+    Arguments:
+    ---------
+    file_path
+        Path where to save dataset and read it from.
+
+    Returns
+    -------
+    Returns `adata` object
+    """
+    adata = read(file_path, backup_url=url_adata_murine, sparse=True, cache=True)
+    return adata
