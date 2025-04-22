67eab1790ad93d8f22f1c2b7e05e748f27e221ab
WWXKenmo <ken71198@hotmail.com>
2025-04-22 22:47:23 +0200

    Add new dataset into regvelo

--- regvelo/datasets/_datasets.py
+++ regvelo/datasets/_datasets.py
@@ -10,7 +10,7 @@ from scvelo.read_load import load
 
 url_adata = "https://drive.google.com/uc?id=1Nzq1F6dGw-nR9lhRLfZdHOG7dcYq7P0i&export=download"
 url_grn = "https://drive.google.com/uc?id=1ci_gCwdgGlZ0xSn6gSa_-LlIl9-aDa1c&export=download/"
-url_adata_murine = "https://drive.google.com/uc?id=19bNQfW3jMKEEjpjNdUkVd7KDTjJfqxa5&export=download"
+url_adata_murine = "https://drive.usercontent.google.com/download?id=19bNQfW3jMKEEjpjNdUkVd7KDTjJfqxa5&export=download&authuser=1&confirm=t&uuid=4fdf3051-229b-4ce2-b644-cb390424570a&at=APcmpoxgcuZ5r6m6Fb6N_2Og6tEO:1745354679573"
 
 def zebrafish_nc(file_path: Union[str, Path] = "data/zebrafish_nc/adata_zebrafish_preprocessed.h5ad"):
     """Zebrafish neural crest cells.
