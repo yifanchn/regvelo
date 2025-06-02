746371399a56765b991fedf08de61efce42bd463
Yifan Chen <yifanchen98@hotmail.com>
2025-06-02 14:40:41 +0200

    Update preprocessing folder
    Update preprocess_data function

--- regvelo/preprocessing/__init__.py
+++ regvelo/preprocessing/__init__.py
@@ -1,4 +1,4 @@
-from .preprocess_data import preprocess_data
+from ._preprocess_data import preprocess_data
 from .sanity_check import sanity_check
 from .set_prior_grn import set_prior_grn
 from .filter_genes import filter_genes
