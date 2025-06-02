da587d4dc52d7c3fb6f91a0835e5fb67076484f6
Yifan Chen <yifanchen98@hotmail.com>
2025-06-02 13:45:16 +0200

    Update metrics and plotting folders

--- regvelo/metrics/_utils.py
+++ regvelo/metrics/_utils.py
@@ -41,3 +41,29 @@ def calculate_entropy(prob_matrix : np.ndarray) -> np.ndarray:
 
     entropy = -np.sum(prob_matrix * log_probs, axis=1)
     return entropy
+
+def get_significance(pvalue: float) -> str:
+    """Return significance annotation for a p-value.
+
+    Parameters
+    ----------
+    pvalue
+        P-value to interpret.
+
+    Returns
+    -------
+    str
+        A string indicating the level of significance:
+        "***" for p < 0.001,
+        "**" for p < 0.01,
+        "*" for p < 0.1,
+        "n.s." (not significant) otherwise.
+    """
+    if pvalue < 0.001:
+        return "***"
+    elif pvalue < 0.01:
+        return "**"
+    elif pvalue < 0.1:
+        return "*"
+    else:
+        return "n.s."
