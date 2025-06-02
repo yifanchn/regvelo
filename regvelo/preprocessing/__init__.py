9b808106d14a3416abc5108d5872f3b55d879cd4
Yifan Chen <yifanchen98@hotmail.com>
2025-06-02 14:46:39 +0200

    Update preprocessing folder
    Update sanity_check function

--- regvelo/preprocessing/__init__.py
+++ regvelo/preprocessing/__init__.py
@@ -1,5 +1,5 @@
 from ._preprocess_data import preprocess_data
-from .sanity_check import sanity_check
+from ._sanity_check import sanity_check
 from .set_prior_grn import set_prior_grn
 from .filter_genes import filter_genes
 
