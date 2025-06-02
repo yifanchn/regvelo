1a3df7073d406b0814c152c4b4e421578f1bbba7
Yifan Chen <yifanchen98@hotmail.com>
2025-06-02 14:03:05 +0200

    Removed pl.fate_probabilities

--- regvelo/plotting/__init__.py
+++ regvelo/plotting/__init__.py
@@ -1,9 +1,7 @@
-from .fate_probabilities import fate_probabilities
 from .commitment_score import commitment_score
 from .depletion_score import depletion_score
 
 __all__ = [
-        "fate_probabilities",
         "commitment_score",
         "depletion_score",
         ]
