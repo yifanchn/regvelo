6171b1a3b31bbf835085780488184aee75ea9689
Yifan Chen <yifanchen98@hotmail.com>
2025-07-15 18:14:39 +0200

    Update pp.filter_genes and pp.preprocess_data
    Removed unused package imports and restructured preprocess_data function.

--- regvelo/preprocessing/_filter_genes.py
+++ regvelo/preprocessing/_filter_genes.py
@@ -1,6 +1,4 @@
-import numpy as np
 import pandas as pd
-
 from anndata import AnnData
 
 def filter_genes(adata: AnnData) -> AnnData:
@@ -24,33 +22,27 @@ def filter_genes(adata: AnnData) -> AnnData:
 
     - ``adata.uns["skeleton"]``.
     """
-    # Initial filtering based on regulators
-    var_mask = adata.var_names.isin(adata.uns["regulators"])
 
-    # Filter genes based on `full_names`
-    adata = adata[:, var_mask].copy()
-
-    # Update skeleton matrix
     skeleton = adata.uns["skeleton"].loc[adata.var_names.tolist(), adata.var_names.tolist()]
     adata.uns["skeleton"] = skeleton
-
-    # Iterative refinement
+    
+    # Remove genes with no upstream regulators
     while adata.uns["skeleton"].sum(0).min() == 0:
         # Update filtering based on skeleton
         skeleton = adata.uns["skeleton"]
         mask = skeleton.sum(0) > 0
 
-        regulators = adata.var_names[mask].tolist()
-        print(f"Number of genes: {len(regulators)}")
+        genes = adata.var_names[mask].tolist()
+        print(f"Number of genes: {len(genes)}")
 
         # Filter skeleton and update `adata`
-        skeleton = skeleton.loc[regulators, regulators]
+        skeleton = skeleton.loc[genes, genes]
         adata.uns["skeleton"] = skeleton
 
         # Update adata with filtered genes
         adata = adata[:, mask].copy()
-        adata.uns["regulators"] = regulators
-        adata.uns["targets"] = regulators
+        adata.uns["regulators"] = genes
+        adata.uns["targets"] = genes
 
         # Re-index skeleton with updated gene names
         skeleton_df = pd.DataFrame(
