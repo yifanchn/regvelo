b62388c659be65a568792a44c2da2c41327d64f2
Ken Wang <ken71198@hotmail.com>
2025-08-17 21:45:53 +0200

    Update _datasets.py
    add hindbrain grn

--- src/regvelo/datasets/_datasets.py
+++ src/regvelo/datasets/_datasets.py
@@ -8,13 +8,19 @@ from scanpy import read
 # Remote data URLs
 url_adata = "https://drive.google.com/uc?id=1Nzq1F6dGw-nR9lhRLfZdHOG7dcYq7P0i&export=download"
 url_grn = "https://drive.google.com/uc?id=1ci_gCwdgGlZ0xSn6gSa_-LlIl9-aDa1c&export=download/"
+
 url_adata_murine_processed = "https://drive.usercontent.google.com/download?id=19bNQfW3jMKEEjpjNdUkVd7KDTjJfqxa5&export=download&authuser=1&confirm=t&uuid=4fdf3051-229b-4ce2-b644-cb390424570a&at=APcmpoxgcuZ5r6m6Fb6N_2Og6tEO:1745354679573"
 url_adata_murine_normalized = "https://drive.usercontent.google.com/download?id=1xy2FNYi6Y2o_DzXjRmmCtARjoZ97Ro_w&export=download&authuser=1&confirm=t&uuid=12cf5d23-f549-48d9-b7ec-95411a58589f&at=APcmpoyexgouf243lNygF9yRUkmi:1745997349046"
 url_adata_murine_velocyto = "https://drive.usercontent.google.com/download?id=18Bhtb7ruoUxpNt8WMYSaJ1RyoiHOCEjd&export=download&authuser=1&confirm=t&uuid=ecc42202-bc82-4ab1-b2c3-bfc31c99f0df&at=APcmpozsh6tBzkv8NSIZW0VipDJa:1745997422108"
+
 url_adata_humanlimb = "https://drive.usercontent.google.com/download?id=17ZaqWx91w--iTtwWMR-SCh6wZO3bzMtM&export=download&authuser=0&confirm=t&uuid=7bdbe7b7-380d-4075-bbe2-1bb65fc0b0a2&at=AN8xHoor6c_hR8_eEd7EENc6dV9M:1750766911809"
+
+url_hindbrain_grn = "https://drive.usercontent.google.com/download?id=11H_7YDhK6EmgZUd6tiyrPBsjl6ASRxH4&export=download&authuser=0&confirm=t&uuid=07c28f5a-bdb5-44ac-93d2-a190e0224e05&at=AN8xHoqX5ROfB0ojYrzisLWDAlnO:1755459638605"
 url_adata_hindbrain = "https://drive.usercontent.google.com/download?id=1g0TE-Fx1DxqLmZe5Hp1F6hYWXq4gpa6N&export=download&authuser=1&confirm=t&uuid=1b60b506-43df-4632-a5a6-f4b1f1780a5f&at=AN8xHopDbZ1_ZR20eBBMGi33rse-:1754605312727"
 url_adata_hindbrain_processed = "https://drive.usercontent.google.com/download?id=1F2A3kUcHmYJR3v61u6Py3Bmo2Zxl4eUC&export=download&authuser=1&confirm=t&uuid=577ac34b-4fb7-49b9-91e9-9868a9e0f9e4&at=AN8xHorPStlDZePwNziyUuAJmZSQ:1754605238549"
+
 url_adata_schwann = "https://drive.usercontent.google.com/download?id=1ZpG1gz0jjPNuXc_bsz3XInVtHooHU02M&export=download&authuser=1&confirm=t&uuid=eeed9d4b-c938-40c9-af24-b248fd29e161&at=AN8xHoqO3RF2oroUCmn7jFURR16T:1754642525195"
+
 
 
 
@@ -134,6 +140,21 @@ def hindbrain(data_type: Literal["original", "preprocessed"] = "preprocessed") -
 
     return adata
 
+def hindbrain_grn(file_path: str | Path = "data/hindbrain/prior_GRN.csv") -> pd.DataFrame:
+    r"""Load prior gene regulatory network (GRN) for human embryonic hindbrain.
+
+    Parameters
+    ---------
+    file_path
+        Path to save the GRN dataset locally and to read it from.
+
+    Returns
+    -------
+    DataFrame representing the GRN.
+    """
+    grn = pd.read_csv(url_hindbrain_grn, sep="\t")
+    grn.to_csv(file_path)
+    return grn
 
 def schwann(file_path: str | Path = "data/schwann/adata_schwann_preprocessed.h5ad") -> AnnData:
     """Load mouse Schwann cell scRNA-seq dataset.
